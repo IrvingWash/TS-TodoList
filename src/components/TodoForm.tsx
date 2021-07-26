5c4b201aff2860735a95452288eb13498e791b0a
Washington Irving <iamwashingyou@gmail.com>
2021-07-26 10:58:57 +0300

    implement two-way binding on todo input

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,7 +1,29 @@
+import React, { useState } from 'react';
+
 export const TodoForm = () => {
+  const [todo, setTodo] = useState<string>('');
+
+  const inputChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTodo(event.target.value);
+  };
+
+  const todoSubmitHandler = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      console.log(todo);
+      setTodo('');
+    }
+  };
+
   return (
     <div className='input-field mt2'>
-      <input type='text' id='title' placeholder='Enter a todo' />
+      <input
+        type='text'
+        id='title'
+        placeholder='Enter a todo'
+        onChange={inputChangeHandler}
+        onKeyPress={todoSubmitHandler}
+        value={todo}
+      />
       <label htmlFor='title' className='active'>
         Enter a todo
       </label>
