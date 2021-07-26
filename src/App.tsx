752ecbd5fb9de8bb430e3891b04a05ba5b07e28d
Washington Irving <iamwashingyou@gmail.com>
2021-07-26 12:22:09 +0300

    add local-storage cashing

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { ITodo } from './intefaces';
 import { Navbar } from './components/Navbar';
 import { TodoForm } from './components/TodoForm';
@@ -8,6 +8,16 @@ declare var confirm: (question: string) => boolean;
 
 const App = () => {
   const [todos, setTodos] = useState<ITodo[]>([]);
+
+  useEffect(() => {
+    const saved = JSON.parse(localStorage.getItem('todos') || '[]') as ITodo[];
+
+    setTodos(saved);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
 
   const addTodoHandler = (title: string) => {
     const newTodo: ITodo = {
