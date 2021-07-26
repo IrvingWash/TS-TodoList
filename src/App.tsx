188e6b2ed4b166a5aa344d4f7efb1a09ab817745
Washington Irving <iamwashingyou@gmail.com>
2021-07-26 11:28:52 +0300

    add todolist component and logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { useState } from 'react';
+import { ITodo } from './intefaces';
 import { Navbar } from './components/Navbar';
 import { TodoForm } from './components/TodoForm';
+import { TodoList } from './components/TodoList';
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState<ITodo[]>([]);
 
   const addTodoHandler = (title: string) => {
-    console.log('Add new todo ', title);
+    const newTodo: ITodo = {
+      title: title,
+      id: Date.now(),
+      completed: false,
+    };
+    setTodos((prevTodos) => {
+      return [newTodo, ...prevTodos];
+    });
   };
 
   return (
@@ -14,6 +23,7 @@ const App = () => {
       <Navbar />
       <div className='container'>
         <TodoForm onAddTodo={addTodoHandler} />
+        <TodoList todos={todos} />
       </div>
     </>
   );
